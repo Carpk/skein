b593f2b496b81e67263a517b6910511e3e60d5d6
Shawn Klein <zappeln@gmail.com>
2015-01-18 00:52:30 -0600

    added JS for AJAX requests and form updating test on webpage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,7 @@
 
 $( document ).ready(function() {
   testData = 'not assigned'
+  count = 0
 
   $(".game-button").click(function(e) {
     e.preventDefault()
@@ -25,6 +26,36 @@ $( document ).ready(function() {
       testData = data
       $(".text-field").append(data["position"]);
     })
-
   })
+
+
+  $('#user-input').keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+      userData = $("#user-input").serializeArray();
+      // $("#user-input")[0].reset();
+      // document.getElementById("user-input").reset();
+      // $("#user-input")[0].reset();
+      $(".text-field").append( userData[0].value + "<br>");
+      $.post( "/welcome", userData, function( data ) {
+
+      })
+      console.log()
+    }
+  });
+
+  // $('.input-field').keydown(function(e) {
+  // var key = e.which;
+  // if (key == 13) {
+  //   // count ++
+  //     $(".text-field").append(count);
+  //   }
+  // });
+
+  // $('#text_field_1').keypress(function(e) {
+  // if (e.which == 13) {
+  // alert('The enter key was pressed!');
+  // }
+  // });
+
 })
